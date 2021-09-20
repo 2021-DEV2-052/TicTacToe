14accd3b282d30dc514b45f5468fa8a79f3c211f
2021-DEV2-052 <2021-DEV2-052@github.com>
2021-09-20 19:44:18 +0200

    RED extra screen state tests

--- app/src/androidTest/java/be/kata/tictactoe/TicTacToeInfoTextTest.kt
+++ app/src/androidTest/java/be/kata/tictactoe/TicTacToeInfoTextTest.kt
@@ -2,6 +2,7 @@ package be.kata.tictactoe
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import be.kata.game.SquareAlreadyClaimedException
@@ -23,5 +24,28 @@ class TicTacToeInfoTextTest {
             )
         }
         composeRule.onNode(hasText("already claimed", true)).assertIsDisplayed()
+    }
+
+    @ExperimentalFoundationApi
+    @Test
+    fun testScreenHasInfoWhenClickingEndedGame() {
+        composeRule.setContent {
+            TicTacToeInfoText(
+                TicTacToeActivity.ErrorState(exception = SquareAlreadyClaimedException(""))
+            )
+        }
+        composeRule.onNode(hasText("has ended", true)).assertIsDisplayed()
+    }
+
+    @ExperimentalFoundationApi
+    @Test
+    fun testScreenHasNoInfoWhenClickingNormalState() {
+        composeRule.setContent {
+            TicTacToeInfoText(
+                TicTacToeActivity.ErrorState(exception = SquareAlreadyClaimedException(""))
+            )
+        }
+        composeRule.onNode(hasText("already claimed", true)).assertIsNotDisplayed()
+        composeRule.onNode(hasText("has ended", true)).assertIsNotDisplayed()
     }
 }
