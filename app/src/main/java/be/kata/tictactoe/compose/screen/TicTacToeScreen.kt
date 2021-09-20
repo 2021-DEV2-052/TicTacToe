5169ecb4e121d7e272aa32b78c010892d4b2366f
2021-DEV2-052 <2021-DEV2-052@github.com>
2021-09-20 16:37:49 +0200

    GREEN tests for right imageState

--- app/src/main/java/be/kata/tictactoe/compose/screen/TicTacToeScreen.kt
+++ app/src/main/java/be/kata/tictactoe/compose/screen/TicTacToeScreen.kt
@@ -1,7 +1,9 @@
 package be.kata.tictactoe.compose.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -10,9 +12,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import be.kata.game.Claimed
+import be.kata.game.Nothing
+import be.kata.game.TicTacToeGame
 import be.kata.game.TicTacToeSquare
+import be.kata.tictactoe.R
 
 @Composable
 fun TicTacToeSquareView(
@@ -28,5 +35,32 @@ fun TicTacToeSquareView(
             .background(color = Color.Gray, shape = RoundedCornerShape(4.dp))
             .clickable(onClick = { onClick(ordinal) })
     ) {
+        when (squareState) {
+            Nothing -> {
+                Image(
+                    painter = painterResource(id = R.drawable.empty),
+                    contentDescription = "nothing",
+                    modifier = Modifier.fillMaxSize().background(Color.Transparent)
+                )
+            }
+            is Claimed -> {
+                when (squareState.player) {
+                    TicTacToeGame.Player.PLAYER_1 -> {
+                        Image(
+                            painter = painterResource(id = R.drawable.cross),
+                            contentDescription = "x",
+                            modifier = Modifier.fillMaxSize().background(Color.Transparent)
+                        )
+                    }
+                    TicTacToeGame.Player.PLAYER_2 -> {
+                        Image(
+                            painter = painterResource(id = R.drawable.circle),
+                            contentDescription = "0",
+                            modifier = Modifier.fillMaxSize().background(Color.Transparent)
+                        )
+                    }
+                }
+            }
+        }
     }
 }
