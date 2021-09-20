492db22a1a8e9d4c9fbfb93c7379905e662eb779
2021-DEV2-052 <2021-DEV2-052@github.com>
2021-09-20 16:34:13 +0200

    GREEN test square ordinal click

--- app/src/main/java/be/kata/tictactoe/compose/screen/TicTacToeScreen.kt
+++ app/src/main/java/be/kata/tictactoe/compose/screen/TicTacToeScreen.kt
@@ -1,6 +1,14 @@
 package be.kata.tictactoe.compose.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -8,6 +16,15 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun TicTacToeSquareView(
     ordinal: Int,
+    size: Dp = (LocalConfiguration.current.screenWidthDp / 3).dp,
     onClick: (Int) -> Unit,
 ) {
+    Surface(
+        modifier = Modifier
+            .size(size)
+            .padding(4.dp)
+            .background(color = Color.Gray, shape = RoundedCornerShape(4.dp))
+            .clickable(onClick = { onClick(ordinal) })
+    ) {
+    }
 }
