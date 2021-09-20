f0f7bca9e18f34c2a96a2af3b834b627d40e1d18
2021-DEV2-052 <2021-DEV2-052@github.com>
2021-09-20 11:31:42 +0200

    GREEN tests for all possible win combos

--- game/src/main/kotlin/be/kata/game/TicTacToeGame.kt
+++ game/src/main/kotlin/be/kata/game/TicTacToeGame.kt
@@ -4,7 +4,11 @@ class TicTacToeGame {
 
     companion object {
         const val FIELD_SIZE = 9
-        private val WIN_COMBO = listOf(0, 1, 2)
+        private val WIN_COMBOS = setOf(
+            setOf(0, 1, 2), setOf(3, 4, 5), setOf(6, 7, 8), // horizontal
+            setOf(0, 3, 6), setOf(1, 4, 7), setOf(2, 5, 8), // vertical
+            setOf(0, 4, 8), setOf(2, 4, 6) // diagonal
+        )
     }
 
     private val gameField: Array<TicTacToeSquare> = Array(FIELD_SIZE) { Nothing }
@@ -46,9 +50,11 @@ class TicTacToeGame {
     }
 
     private fun winComboMatches() =
-        WIN_COMBO
-            .map { squareOrdinal -> gameField[squareOrdinal] }
-            .all { square -> square is Claimed && square.player == activePlayer }
+        WIN_COMBOS
+            .any { winCombo ->
+                 winCombo.map { squareOrdinal -> gameField[squareOrdinal] }
+                .all { square -> square is Claimed && square.player == activePlayer }
+            }
 
     private fun allSquaresClaimed() = gameField.all { square -> square is Claimed }
 
