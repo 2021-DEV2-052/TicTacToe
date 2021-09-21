c59adcbadb44f141ce9b4d7b9e010a6896939dcc
2021-DEV2-052 <2021-DEV2-052@github.com>
2021-09-21 08:55:25 +0200

    GREEN test initial Game State is Playing

--- app/src/main/java/be/kata/tictactoe/ui/GameViewModel.kt
+++ app/src/main/java/be/kata/tictactoe/ui/GameViewModel.kt
@@ -1,7 +1,6 @@
 package be.kata.tictactoe.ui
 
 import androidx.lifecycle.ViewModel
-import be.kata.game.Nothing
 import be.kata.game.TicTacToeGame
 import be.kata.tictactoe.TicTacToeActivity
 import be.kata.tictactoe.data.GameCreator
@@ -10,10 +9,11 @@ import kotlinx.coroutines.flow.StateFlow
 
 class GameViewModel(private val gameCreator: GameCreator) : ViewModel() {
 
-    val gameState: StateFlow<TicTacToeGame.State> =
-        MutableStateFlow(TicTacToeGame.State(List(9) { Nothing }, TicTacToeGame.Draw))
+    private val game = gameCreator.createGame()
 
-    private val _uiState: MutableStateFlow<TicTacToeActivity.UIState> =
-        MutableStateFlow(TicTacToeActivity.Normal)
+    private val _gameState = MutableStateFlow(game.state)
+    val gameState: StateFlow<TicTacToeGame.State> = _gameState
+
+    private val _uiState = MutableStateFlow(TicTacToeActivity.Normal)
     val uiState: StateFlow<TicTacToeActivity.UIState> = _uiState
 }
--- app/src/test/java/be/kata/tictactoe/GameViewModelTest.kt
+++ app/src/test/java/be/kata/tictactoe/GameViewModelTest.kt
@@ -4,13 +4,16 @@ import be.kata.game.TicTacToeGame
 import be.kata.tictactoe.ui.GameViewModel
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 
 class GameViewModelTest {
 
     @Test
     fun gameUIWillStartNormal() {
-        val viewModel = GameViewModel(mock())
+        val viewModel = GameViewModel(mock {
+            on { createGame() } doReturn TicTacToeGame()
+        })
         assertEquals(
             "UI should start in normal state",
             TicTacToeActivity.Normal,
@@ -20,11 +23,13 @@ class GameViewModelTest {
 
     @Test
     fun gameBoardWillBeExposed() {
-        val viewModel = GameViewModel(mock())
+        val viewModel = GameViewModel(mock {
+            on { createGame() } doReturn TicTacToeGame()
+        })
         assertEquals(
             "Game should start in playing state",
-            TicTacToeActivity.Normal,
-            viewModel.gameState.value.status == TicTacToeGame.Playing(TicTacToeGame.Player.PLAYER_1)
+            TicTacToeGame.Playing(TicTacToeGame.Player.PLAYER_1),
+            viewModel.gameState.value.status
         )
     }
 }
